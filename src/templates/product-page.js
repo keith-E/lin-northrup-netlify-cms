e8c54c95bf29df5cc8456a1012da2d41ea92b0ba
Keith J Eagleson <keagleson28@gmail.com>
2020-10-30 11:43:28 -0400

    Removed full images, core heading, descriptions and pricing

--- src/templates/product-page.js
+++ src/templates/product-page.js
@@ -4,49 +4,17 @@ import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Features from '../components/Features'
 import Testimonials from '../components/Testimonials'
-import Pricing from '../components/Pricing'
 import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
 
 export const ProductPageTemplate = ({
-  image,
-  title,
-  heading,
-  description,
   intro,
   main,
   testimonials,
-  fullImage,
-  pricing,
 }) => (
     <div className="content">
-      <div
-        className="full-width-image-container margin-top-0"
-        style={{
-          backgroundImage: `url(${!!image.childImageSharp ? image.childImageSharp.fluid.src : image
-            })`,
-        }}
-      >
-        <h2
-          className="has-text-weight-bold is-size-1"
-          style={{
-            boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-            backgroundColor: '#f40',
-            color: 'white',
-            padding: '1rem',
-          }}
-        >
-          {title}
-        </h2>
-      </div>
       <section className="section section--gradient">
         <div className="container">
           <div className="section">
-            <div className="columns">
-              <div className="column is-7 is-offset-1">
-                <h3 className="has-text-weight-semibold is-size-2">{heading}</h3>
-                <p>{description}</p>
-              </div>
-            </div>
             <div className="columns">
               <div className="column is-10 is-offset-1">
                 <Features gridItems={intro.blurbs} />
@@ -80,20 +48,6 @@ export const ProductPageTemplate = ({
                   </div>
                 </div>
                 <Testimonials testimonials={testimonials} />
-                <div
-                  className="full-width-image-container"
-                  style={{
-                    backgroundImage: `url(${fullImage.childImageSharp
-                        ? fullImage.childImageSharp.fluid.src
-                        : fullImage
-                      })`,
-                  }}
-                />
-                <h2 className="has-text-weight-semibold is-size-2">
-                  {pricing.heading}
-                </h2>
-                <p className="is-size-5">{pricing.description}</p>
-                <Pricing data={pricing.plans} />
               </div>
             </div>
           </div>
@@ -103,10 +57,6 @@ export const ProductPageTemplate = ({
   )
 
 ProductPageTemplate.propTypes = {
-  image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-  title: PropTypes.string,
-  heading: PropTypes.string,
-  description: PropTypes.string,
   intro: PropTypes.shape({
     blurbs: PropTypes.array,
   }),
@@ -118,12 +68,6 @@ ProductPageTemplate.propTypes = {
     image3: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
   }),
   testimonials: PropTypes.array,
-  fullImage: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-  pricing: PropTypes.shape({
-    heading: PropTypes.string,
-    description: PropTypes.string,
-    plans: PropTypes.array,
-  }),
 }
 
 const ProductPage = ({ data }) => {
@@ -133,14 +77,9 @@ const ProductPage = ({ data }) => {
     <Layout>
       <ProductPageTemplate
         image={frontmatter.image}
-        title={frontmatter.title}
-        heading={frontmatter.heading}
-        description={frontmatter.description}
         intro={frontmatter.intro}
         main={frontmatter.main}
         testimonials={frontmatter.testimonials}
-        fullImage={frontmatter.full_image}
-        pricing={frontmatter.pricing}
       />
     </Layout>
   )
