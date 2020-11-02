f5689f17723db5794899421cc287aa81178cd223
Keith J Eagleson <keagleson28@gmail.com>
2020-11-02 00:27:23 -0500

    iPad sucks2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
       >
         <div className="container">
           <div className="navbar-brand">
-            <Link to="/" className="navbar-item" title="Home" style={{ fontFamily: 'Marcellus SC', fontWeight: '600', fontSize: '22px', width: '330px', color: '#3e734c' }}>
+            <Link to="/" className="navbar-item" title="Home" style={{ fontFamily: 'Marcellus SC', fontWeight: '600', fontSize: '22px', width: '400px', color: '#3e734c' }}>
               Leela and the Forest of Light
             </Link>
             {/* Hamburger menu */}
